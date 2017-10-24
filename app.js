d6669c880225ecdc3c909bf5fd07ee22ff1abe4a
lily <strewb89@gmail.com>
2017-10-24 10:32:41 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,20 +1,15 @@
 var http = require('http');
 var express = require('express');
-
 var app = express();
 var server = http.createServer(app);
 var io = require('socket.io')(server);
-
 app.use(express.static('static'));
-
-// store in memory
-var games = {};
-var playerColors = ['rgb(200, 0, 0)', 'rgb(0, 0, 200)']; //, 'rgba(0, 200, 0, 0.8)', 'rgba(0, 200, 200, 0.8)'];
+var games = {}; // store in memory
+var playerColors = ['rgb(200, 0, 0)', 'rgb(0, 0, 200)']; 
 
 function getOrCreateGame(gameName) {
   var game = games[gameName];
   if (game) return game;
-
   console.log('creating new game', gameName);
   return games[gameName] = {
     hostSocket: null,
@@ -26,14 +21,12 @@ function leaveGame(gameName, leavingPlayer) {
   var game = games[gameName];
   if (!game) return console.log('no game with name', gameName, 'to leave');
   game.players = game.players.filter(function(player) { return player.player !== leavingPlayer; });
-
   if (game.hostSocket) game.hostSocket.emit('player left', leavingPlayer);
 }
 
 io.on('connection', function(socket) {
   var hostedGames = [];
   var joinedGames = [];
-
   console.log('user connected');
   socket.on('disconnect', function() {
     console.log('user disconnected');
@@ -52,16 +45,13 @@ io.on('connection', function(socket) {
     console.log('host game', data);
     var game = getOrCreateGame(data.gameName);
     game.hostSocket = socket;
-
     hostedGames.push(data.gameName);
-
     game.players.forEach(function(p) { socket.emit('new player joined', p.player); });
   });
 
   socket.on('join game', function(data) {
     console.log('join game', data);
     var game = getOrCreateGame(data.gameName);
-
     var num = 1;
     game.players.forEach(function(p) { if (p.player.num === num) num += 1; });
     var player = {
@@ -74,17 +64,16 @@ io.on('connection', function(socket) {
       player: player,
       socket: socket
     });
+    
     joinedGames.push({ gameName: data.gameName, player: player });
-
     console.log('new player joined', player);
-
     if (game.hostSocket) game.hostSocket.emit('new player joined', player);
     socket.emit('game joined', player);
   });
 });
 
 var port = process.env.PORT || 5000;
-server.listen(port, function(err) {
-  if (err) return console.log(err.stack);
-  console.log('listening on port', port);
+ server.listen(port, function(err) {
+ if (err) return console.log(err.stack);
+ console.log('listening on port', port);
 })
